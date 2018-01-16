5c5a338791b73f3655ec84fe20dec9ed12e0b8b4
ITxiaowu <1028402351@qq.com>
2018-01-16 18:07:43 +0800

    Revert "删除多余文件"
    This reverts commit e23c1db1ec200f5e403fbfa5320422fd8bb58fb4.

---
+++ src/main/java/com/example/model/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.example.model.mapper;
+
+import com.example.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("select * from user")
+    List<User> getUsetList();
+
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
