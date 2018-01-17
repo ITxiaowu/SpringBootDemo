6b7f91d339da2f8d02538eea4b5229182f602065
ITxiaowu <1028402351@qq.com>
2018-01-17 17:53:37 +0800

    分页插件
    分页插件添加...未关联..待补充

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//@MapperScan("microservice.qssj.mapper")// 也可以在每个mapper上添加@Mapper注释,否则会出现扫描不到mapper接口的情况
+@EnableTransactionManagement// 如果mybatis中service实现类中加入事务注解，需要此处添加该注解
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
